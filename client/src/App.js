ae339a60f450d5775342d5707cdd3888b35a22d6
Saif Ayari <saifayari312@gmail.com>
2024-08-05 11:51:25 +0100

    setup

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme } from './utils/Themes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+width:100% 
+height:100%
+display:flex
+flex-direction: column
+background :${({ theme }) => theme.bg}
+color: ${({ theme }) => theme.text_primary}
+overflow-x: hidden
+overflow-y: hidden
+transition:all 0.2s ease`;
+
+export default function App() {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+            <Navbar />
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
